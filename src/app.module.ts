0717fc93814fbfe6b389f4322d7fe89b1a4aec5c
farzeen-ali <farzeenaliarif786@gmail.com>
2025-07-23 19:09:41 +0500

    CRUD App in NestJS using GraphQL Prisma ORM and NEON PostgreSQL DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { BookModule } from './book/book.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    sortSchema: true,
+    playground: true
+  }),PrismaModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
